5bf0b5e233d4ebca2e726c2d37113a7bb3db56b0
Dirga Yasa <cekingx@gmail.com>
2024-08-23 11:21:27 +0800

    feat: setup view

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port);
+  console.log(`app run on port: ${port}`);
 }
 bootstrap();
